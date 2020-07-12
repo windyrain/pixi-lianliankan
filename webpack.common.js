24976d557ef23e4a9a49d4bf3c505b7d28162d0a
fengyu <fengyu@ksjgs.com>
2020-07-12 21:41:51 +0800

    feat: 支持 ts 语法

--- webpack.common.js
+++ webpack.common.js
@@ -6,14 +6,18 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
-  entry: ['./js/main.js'],
+  entry: ['./js/main.ts'],
   // mode: 'none', // none development production
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'game.min.[hash:8].js',
   },
   target: 'web',
-  
+  module: {
+    rules: [
+        {test:/\.ts$/,exclude: /node_modules/,use:['ts-loader']}
+    ]
+  },  
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
@@ -41,5 +45,8 @@ module.exports = {
       file:path.join(__dirname,'dist','index.html'),
       template:'./index.html'
     })
-  ]
+  ],
+  resolve: {
+    extensions:['.js','.ts']
+  }
 }
